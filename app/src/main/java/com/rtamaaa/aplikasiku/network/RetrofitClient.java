9c055e8cb62d04d5db3c78fcf138968b04ef0424
rtamaaa <pratama123oke@gmail.com>
2025-05-23 13:49:32 +0700

    Login menggunakan REST-API

--- app/src/main/java/com/rtamaaa/aplikasiku/network/RetrofitClient.java
+++ app/src/main/java/com/rtamaaa/aplikasiku/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://10.0.2.2/api_andro/";
+    private static final String BASE_URL = "https://5d1c-140-213-7-110.ngrok-free.app/api_andro/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
